3eb9b9e6413e09e1734c1feec28dd0eac712a283
Luka Žohar <luka.zohi@gmail.com>
2020-01-30 22:45:35 +0100

    Added files to start app serve and open code with project.

---
+++ serve-f1-overview.bat
@@ -0,0 +1,5 @@
+start cmd /k call "./start_files/start_app.cmd" 4444
+start cmd /k call "./start_files/start_code.cmd" 5555
+start cmd /k call "./start_files/start_git_gui.cmd" 6666
+
+exit 0
---
+++ start_files/start_app.cmd
@@ -0,0 +1,1 @@
+ng serve --watch --open --port 4200
