ff9d491121017eb13d155863ad2da99c3f190673
Luka Žohar <luka.zohi@gmail.com>
2020-02-15 18:14:15 +0100

    Moved bootstrap module from modules folder to app folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BootstrapModule } from './modules/bootstrap/bootstrap.module';
+import { BootstrapModule } from './bootstrap.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
