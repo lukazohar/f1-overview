8922b15526ad4f85546ecb97db9fe7fed6ebdb4c
Luka Žohar <luka.zohi@gmail.com>
2020-02-16 20:05:39 +0100

    Added page for not found links. ng g c components/notFoundPage --module=app.module.ts. Redirect from '/' to '/seasons'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'seasons' },
+  { path: 'page-not-found', component: NotFoundPageComponent },
   {
     path: 'seasons',
     loadChildren: () => import('./modules/seasons/seasons.module').then(module => module.SeasonsModule)
-  }
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'page-not-found' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BootstrapModule } from './bootstrap.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, NotFoundPageComponent],
   imports: [BrowserModule, AppRoutingModule, BootstrapModule],
   providers: [],
   bootstrap: [AppComponent]
