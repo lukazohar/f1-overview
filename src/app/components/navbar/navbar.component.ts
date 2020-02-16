3c5639bc401209e613f5cec305eb15fcfe4e3bd5
Luka Žohar <luka.zohi@gmail.com>
2020-02-16 16:13:12 +0100

    Updated navbar and footer

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  isMenuCollapsed = false;
+  isMenuCollapsed = true;
   constructor() {}
 
   ngOnInit() {}
