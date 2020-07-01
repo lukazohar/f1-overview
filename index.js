e6eae6ce88d111484a8502c62da311fe8197c8a8
Luka Žohar <luka.zohi@gmail.com>
2020-07-01 23:36:26 +0200

    Publishing to Heroku

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./docs/'));
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`f1-overview listening at http://localhost:${port}`));
